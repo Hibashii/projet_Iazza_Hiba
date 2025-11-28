9d19a78f1cd552a48d7a8f9117a675a976b17cf4
Hibashii <98167346+Hibashii@users.noreply.github.com>
2025-11-28 10:52:11 +0100

    first push

---
+++ src/main/java/org/example/projet_iazza_hiba/ProjetIazzaHibaApplication.java
@@ -0,0 +1,13 @@
+package org.example.projet_iazza_hiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetIazzaHibaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetIazzaHibaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projet_iazza_hiba/ProjetIazzaHibaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projet_iazza_hiba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetIazzaHibaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
